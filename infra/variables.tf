32cf3341504a1179493de67893d3ea0c0ffaad48
Alejandro <18149180+malandro-sv@users.noreply.github.com>
2025-12-01 12:00:46 -0600

    Create variables.tf
    variables is invoked by main.tf

---
+++ infra/variables.tf
@@ -0,0 +1,18 @@
+variables "project_id" {
+  description = "gilroy was here"
+  type        = string
+  default     = project-id-goes-here
+}
+
+variable "region" {
+  description = "some region"
+  type        = string
+  default     = region_goes_here
+}
+
+variable "zone" {
+  description = "some region"
+  type        = string
+  default     = region_goes_here-a
+}
+
